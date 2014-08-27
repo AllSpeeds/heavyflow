db64644825b97fbacf4cf74d1c48cf1d3101059b
Chad Gowan <cgowan@spredfast.com>
2014-08-26 21:33:06 -0500

    Changing to 17 and adding some logging with total

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 var Gpio = require('onoff').Gpio;
-var gpioNum = 12;
+var gpioNum = 17;
 var fmeter = new Gpio(gpioNum, 'in', 'rising');
+var total = 0;
 
 console.log('Starting to watch for GPIO #' + gpioNum);
 
 fmeter.watch(function(err, value) {
 	if(err) {
+		console.log(err);
+
 		exit();
 	}
 
-	console.log(value);
+	total = total + value;
+	console.log(total);
 });
 
 function exit() {
 	console.log('exiting...');
-	
+
 	fmeter.unexport();
 	process.exit();
 }
